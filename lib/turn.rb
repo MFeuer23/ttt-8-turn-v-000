bdd3fed15a3d595e2a4fd18701a9b964927a00dd
Mallory Feuer <mallory.feuer@gmail.com>
2018-01-31 18:48:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
     index = gets.strip
-    input_to_index(index) = x
+    x = input_to_index(index)
   if valid_move?(board, x) == true
     
     move(board, x, player)
